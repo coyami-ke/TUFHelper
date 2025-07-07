4bf540b1052b1d0fa52891de8ef4cf66732e4dd6
Никита Вырыпаев <dropsy422@gmail.com>
2025-07-07 14:09:08 +0500

    added an account system

---
+++ Assets/TUFHelper/ModScripts/Helpers/AccountSaver.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace TUFHelper.Utils
+{
+    public class AccountSaver
+    {
+        public static readonly string PATH_TO_ACCOUNT_FILE = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "TUFHelper", "token.json");
+
+        public string Token { get; set; } = "";
+
+        public static AccountSaver GetAccount()
+        {
+            if (!File.Exists(PATH_TO_ACCOUNT_FILE)) return null;
+            return JsonConvert.DeserializeObject<AccountSaver>(File.ReadAllText(PATH_TO_ACCOUNT_FILE));
+        }
+
+        public void Save()
+        {
+            File.WriteAllText(PATH_TO_ACCOUNT_FILE, JsonConvert.SerializeObject(this));
+        }
+    }
+}
