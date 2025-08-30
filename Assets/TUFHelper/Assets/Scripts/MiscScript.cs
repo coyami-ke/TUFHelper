ad1f8dedc8e489048d0d676123e1c85353557e29
Никита Вырыпаев <dropsy422@gmail.com>
2025-08-30 11:21:17 +0500

    Revert "fixed naming"
    This reverts commit b96996fa6774575a2502c060ae725a912a273d55.

--- Assets/TUFHelper/Assets/Scripts/MiscScript.cs
+++ Assets/TUFHelper/Assets/Scripts/MiscScript.cs
@@ -44,7 +44,7 @@ public class MiscScript : MonoBehaviour
             Main.Setting.Save(Main.ModEntry);
         }
 
-        var fmod = UnityModManager.FindMod("ADOFAI Fmod");
+        var fmod = UnityModManager.FindMod("FMod");
         if (fmod != null && !Main.Setting.IsShowedFmodError)
         {
             var fmodAssembly = fmod.Assembly;
