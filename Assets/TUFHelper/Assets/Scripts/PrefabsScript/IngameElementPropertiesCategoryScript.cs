2fc8ed20b7ed069afd5b71d768d195db55f39b1f
Никита Вырыпаев <dropsy422@gmail.com>
2026-07-02 21:30:10 +0500

    added Category as a property

--- Assets/TUFHelper/Assets/Scripts/PrefabsScript/IngameElementPropertiesCategoryScript.cs
+++ Assets/TUFHelper/Assets/Scripts/PrefabsScript/IngameElementPropertiesCategoryScript.cs
@@ -1,3 +1,4 @@
+using System;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
@@ -8,22 +9,26 @@ public class IngameElementPropertiesCategoryScript : MonoBehaviour
     public Image backgroundImage, lineImage, iconImage;
     public TextMeshProUGUI text;
 
+    public IngameElementSettingsCategory Category { get; private set; }
+
     private bool _isSelected = false;
     public bool IsSelected
     {
         get => _isSelected;
         set
         {
+            if (_isSelected == value) return;
+
             _isSelected = value;
             if (value)
             {
                 lineImage.rectTransform.DOScaleX(1f, 0.5f).SetEase(Ease.OutExpo);
-                backgroundImage.DOColor(new Color(1, 1, 1, 20 / 255f), 0.5f).SetEase(Ease.OutExpo);
+                backgroundImage.DOColor(new Color(1f, 1f, 1f, 20f / 255f), 0.5f).SetEase(Ease.OutExpo);
             }
             else
             {
                 lineImage.rectTransform.DOScaleX(0f, 0.5f).SetEase(Ease.OutExpo);
-                backgroundImage.DOColor(new Color(1, 1, 1, 10 / 255f), 0.5f).SetEase(Ease.OutExpo);
+                backgroundImage.DOColor(new Color(1f, 1f, 1f, 10f / 255f), 0.5f).SetEase(Ease.OutExpo);
             }
         }
     }
@@ -32,10 +37,26 @@ public class IngameElementPropertiesCategoryScript : MonoBehaviour
     {
         text.text = category.DisplayName;
         iconImage.sprite = category.Icon;
+
+        Category = category;
     }
 
     public void OnClicked()
     {
+        if (IsSelected) return;
+
+        if (transform.parent != null)
+        {
+            foreach (Transform sibling in transform.parent)
+            {
+                var siblingScript = sibling.GetComponent<IngameElementPropertiesCategoryScript>();
+                if (siblingScript != null && siblingScript != this)
+                {
+                    siblingScript.IsSelected = false;
+                }
+            }
+        }
+
         IsSelected = true;
     }
 }
