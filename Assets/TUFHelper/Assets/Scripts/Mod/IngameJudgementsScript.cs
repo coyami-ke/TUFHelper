b6707e9acd2889c07aac24d426650a306ce64f45
coyami-ke <dropsy422@gmail.com>
2026-08-19 21:45:27 +0500

    implement Non-XPerfect mode for the IngameJudgements

--- Assets/TUFHelper/Assets/Scripts/Mod/IngameJudgementsScript.cs
+++ Assets/TUFHelper/Assets/Scripts/Mod/IngameJudgementsScript.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using TMPro;
 using TUFHelper;
 using TUFHelper.Utils;
@@ -11,6 +13,7 @@ public class IngameJudgementsScript : BasicIngameElement
     private int tooEarlyCount = 0;
     private int veryEarlyCount = 0;
     private int earlyPerfectCount = 0;
+    private int perfectCount = 0; // Merged count for non-XPerfect mode
     private int plusPerfectCount = 0;
     private int xPerfectCount = 0;
     private int minusPerfectCount = 0;
@@ -19,6 +22,7 @@ public class IngameJudgementsScript : BasicIngameElement
     private int tooLateCount = 0;
     private int missOverloadCount = 0;
     private int missDeathCount = 0;
+    private JudgementsSettingsCategory judgementsSettings;
 
     public override bool IsShownOnlyInTUFHelper => false;
 
@@ -28,11 +32,31 @@ public class IngameJudgementsScript : BasicIngameElement
     public override Anchor DefaultAnchor => Anchor.MiddleBottom;
     public override TextMeshProUGUI[] Texts => new TextMeshProUGUI[1] { text };
 
+    protected override void OnLoadCustomSettings(IngameElementModel model)
+    {
+        if (judgementsSettings != null)
+        {
+            judgementsSettings.PropertyChanged -= JudgementsSettings_PropertyChanged;
+        }
+
+        judgementsSettings = model.GetCategory("Judgements", new JudgementsSettingsCategory());
+        judgementsSettings.PropertyChanged += JudgementsSettings_PropertyChanged;
+    }
+
+    private void JudgementsSettings_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(JudgementsSettingsCategory.UseXPerfectSystem))
+        {
+            UpdateTextDisplay();
+        }
+    }
+
     private void ResetCounters()
     {
         tooEarlyCount = 0;
         veryEarlyCount = 0;
         earlyPerfectCount = 0;
+        perfectCount = 0;
         plusPerfectCount = 0;
         xPerfectCount = 0;
         minusPerfectCount = 0;
@@ -59,20 +83,29 @@ public class IngameJudgementsScript : BasicIngameElement
     {
         if (e.Hit == HitMargin.Auto) return;
 
+        bool useXPerfect = judgementsSettings?.UseXPerfectSystem ?? true;
+
         switch (e.Hit)
         {
             case HitMargin.Perfect:
-                if (e.DetailedJudge == DetailedJudge.XPerfect)
+                if (useXPerfect)
                 {
-                    xPerfectCount++;
+                    if (e.DetailedJudge == DetailedJudge.XPerfect)
+                    {
+                        xPerfectCount++;
+                    }
+                    else if (e.DetailedJudge == DetailedJudge.PlusPerfect)
+                    {
+                        plusPerfectCount++;
+                    }
+                    else if (e.DetailedJudge == DetailedJudge.MinusPerfect)
+                    {
+                        minusPerfectCount++;
+                    }
                 }
-                else if (e.DetailedJudge == DetailedJudge.PlusPerfect)
+                else
                 {
-                    plusPerfectCount++;
-                }
-                else if (e.DetailedJudge == DetailedJudge.MinusPerfect)
-                {
-                    minusPerfectCount++;
+                    perfectCount++;
                 }
                 break;
 
@@ -118,16 +151,53 @@ public class IngameJudgementsScript : BasicIngameElement
     {
         if (text == null) return;
 
-        text.text = $"<color=#ca69ff>{missDeathCount}</color> " +
-                    $"<color=red>{tooEarlyCount}</color> " +
-                    $"<color=orange>{veryEarlyCount}</color> " +
-                    $"<color=yellow>{earlyPerfectCount}</color> " +
-                    $"<color=green>{plusPerfectCount}</color> " +
-                    $"<color=#69afff>{xPerfectCount}</color> " +
-                    $"<color=green>{minusPerfectCount}</color> " +
-                    $"<color=yellow>{latePerfectCount}</color> " +
-                    $"<color=orange>{veryLateCount}</color> " +
-                    $"<color=red>{tooLateCount}</color> " +
-                    $"<color=#ca69ff>{missOverloadCount}</color>";
+        bool useXPerfect = judgementsSettings?.UseXPerfectSystem ?? true;
+
+        if (useXPerfect)
+        {
+            text.text = $"<color=#ca69ff>{missDeathCount}</color> " +
+                        $"<color=red>{tooEarlyCount}</color> " +
+                        $"<color=orange>{veryEarlyCount}</color> " +
+                        $"<color=yellow>{earlyPerfectCount}</color> " +
+                        $"<color=green>{plusPerfectCount}</color> " +
+                        $"<color=#69afff>{xPerfectCount}</color> " +
+                        $"<color=green>{minusPerfectCount}</color> " +
+                        $"<color=yellow>{latePerfectCount}</color> " +
+                        $"<color=orange>{veryLateCount}</color> " +
+                        $"<color=red>{tooLateCount}</color> " +
+                        $"<color=#ca69ff>{missOverloadCount}</color>";
+        }
+        else
+        {
+            text.text = $"<color=#ca69ff>{missDeathCount}</color> " +
+                        $"<color=red>{tooEarlyCount}</color> " +
+                        $"<color=orange>{veryEarlyCount}</color> " +
+                        $"<color=yellow>{earlyPerfectCount}</color> " +
+                        $"<color=green>{perfectCount}</color> " +
+                        $"<color=yellow>{latePerfectCount}</color> " +
+                        $"<color=orange>{veryLateCount}</color> " +
+                        $"<color=red>{tooLateCount}</color> " +
+                        $"<color=#ca69ff>{missOverloadCount}</color>";
+        }
     }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (judgementsSettings != null)
+        {
+            judgementsSettings.PropertyChanged -= JudgementsSettings_PropertyChanged;
+        }
+    }
+}
+
+public partial class JudgementsSettingsCategory : IngameElementSettingsCategory
+{
+    public override string DisplayName => "Judgements";
+    public override Sprite Icon => Main.assets.LoadAsset<Sprite>("assets/tufhelper/assets/sprites/number.png");
+
+    [ObservableProperty]
+    [property: ShowInOverlayerSettings("Use XPerfect")]
+    private bool _useXPerfectSystem = true;
 }
